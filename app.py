5caccd1e7fa68d5304bb4221858cc03e8e013333
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-08 16:04:36 -0500

    uncommented code

--- app.py
+++ app.py
@@ -194,38 +194,38 @@ print(API_KEY)
 TENOR_URL = 'https://api.tenor.com/v1/search'
 pp = PrettyPrinter(indent=4)
 
-# @app.route('/gif_search', methods=['GET', 'POST'])
-# def gif_search():
-#     """Show a form to search for GIFs and show resulting GIFs from Tenor API."""
-#     if request.method == 'POST':
-#         # TODO: Get the search query & number of GIFs requested by the user, store each as a 
-#         # variable
+@app.route('/gif_search', methods=['GET', 'POST'])
+def gif_search():
+    """Show a form to search for GIFs and show resulting GIFs from Tenor API."""
+    if request.method == 'POST':
+        # TODO: Get the search query & number of GIFs requested by the user, store each as a 
+        # variable
 
-#         response = requests.get(
-#             TENOR_URL,
-#             {
-#                 # TODO: Add in key-value pairs for:
-#                 # - 'q': the search query
-#                 # - 'key': the API key (defined above)
-#                 # - 'limit': the number of GIFs requested
-#             })
+        response = requests.get(
+            TENOR_URL,
+            {
+                # TODO: Add in key-value pairs for:
+                # - 'q': the search query
+                # - 'key': the API key (defined above)
+                # - 'limit': the number of GIFs requested
+            })
 
-#         gifs = json.loads(response.content).get('results')
+        gifs = json.loads(response.content).get('results')
 
-#         context = {
-#             'gifs': gifs
-#         }
+        context = {
+            'gifs': gifs
+        }
 
-#          # Uncomment me to see the result JSON!
-#         # Look closely at the response! It's a list
-#         # list of data. The media property contains a 
-#         # list of media objects. Get the gif and use it's 
-#         # url in your template to display the gif. 
-#         # pp.pprint(gifs)
+         # Uncomment me to see the result JSON!
+        # Look closely at the response! It's a list
+        # list of data. The media property contains a 
+        # list of media objects. Get the gif and use it's 
+        # url in your template to display the gif. 
+        # pp.pprint(gifs)
 
-#         return render_template('gif_search.html', **context)
-#     else:
-#         return render_template('gif_search.html')
+        return render_template('gif_search.html', **context)
+    else:
+        return render_template('gif_search.html')
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
